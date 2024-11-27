4c25f2c3ebc781ae878f81c6e1c01e2643b909db
Rabin Pokharel <rabinpokharel222@gmail.com>
2024-11-27 20:13:30 +0545

    moreWorking2

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const input = document.getElementById("input");
 const button = document.getElementsByClassName("remove-task");
 const ul = document.getElementById("task");
+const addButton = document.getElementById("add-button");
 
 
 function addTask(){
     if(input.value !== 0){
         const inputValue = input.value;
         const createButton = document.createElement('button');
+        const creatingListItem = document.createElement('li');
+
         createButton.textContent= 'remove';
-        creatingListItem = document.createElement('li');
         creatingListItem.textContent = inputValue;
-        ul.appendChild(creatingListItem);
-        ul.appendChild(createButton);
-        ul.textcontent = `<li>${inputValue} <button>${createButton}</button></li>`;;
+
+        if(inputValue === ''){
+            console.log("error");
+        }
+        else{
+            ul.appendChild(creatingListItem);
+            ul.appendChild(createButton);
+            ul.textcontent = `<li>${inputValue} <button>${createButton}</button></li>`;
+            input.value = '';
+        }
     }
     else{
         console.log("error");
     }
-    
 }
 
 function removeTask(){
